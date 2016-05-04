4a86674d42f8e19c1befbcc5983f832cab3f1366
wonju <gahuins@gmail.com>
2016-05-04 20:57:58 +0900

    과 제
    별 모양 만들기 과제

---
+++ class/star.py
@@ -0,0 +1,12 @@
+for i in range(5):
+    print("*" * (i + 1))
+
+for i in range(5):
+    print((" " * (5 - i)) + ("*" * (i + 1)))
+
+for i in range(5):
+    print("*" * (5 - i ))
+
+for i in range(5):
+    print((" " * i) + ("*" * (5 - i )))
+
